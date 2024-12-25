e40e65865edd1368c3deccb3c6f1ba0be6f4bb28
minseok Kim <05300903kms@gmail.com>
2024-12-26 01:17:01 +0900

    필요없는 파일 삭제 및 테스트 로드 코드 작성:

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import { createClient } from '@/utils/supabase/client';
+
+export default function Main() {
+  async function loadDb() {
+    const supabase = createClient();
+    //supabase.from('이곳에 테이블의 이름을 넣는다')
+  }
+  return <div>text</div>;
+}
