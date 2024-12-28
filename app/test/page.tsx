2b3e4eb01490ffcef0c9f9b77bcdbd50a854afc6
minseok Kim <05300903kms@gmail.com>
2024-12-28 20:59:32 +0900

    테스트 코드

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,6 +4,7 @@ export default function Main() {
   async function loadDb() {
     const supabase = createClient();
     //supabase.from('이곳에 테이블의 이름을 넣는다')
+    //develop-test-add
   }
   return <div>text</div>;
 }
